03bbea3e4968d2ee681b8dee62a5c869afc8d966
Pragyan Tripathi <pntripathi9417@gmail.com>
2019-04-13 06:19:44 +0530

    chore(clojure-noob): reverted back to initial code

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -6,28 +6,3 @@
   [& args]
   (println "I am a little teapot"))
 
-(println "Cleanliness is next to godliness")
-
-(defn train
-  []
-  (println "Choo Choo!"))
-
-(+ 1  (* 3 4 5))
-
-(str "hello world" "I am awesome")
-
-(if true
-  "I AM TRUE"
-  "I AM FALSE"
-  )
-
-(if false
-  "I AM TRUE"
-  "I AM FALSE")
-
-(def test-str ["Hello", "World", "My"])
-
-(println test-str)
-
-(defn too-enthusiastic
-  [name]
