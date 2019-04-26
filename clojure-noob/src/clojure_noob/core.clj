d5a54db4f36bfe067f4834d550cbcb6333469c1f
Pragyan Tripathi <pntripathi9417@gmail.com>
2019-04-27 00:11:54 +0530

    add peg thing project

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -6,3 +6,28 @@
   [& args]
   (println "I am a little teapot"))
 
+
+(def asym-hobbit-body-parts [{:name "head" :size 3}
+                            {:name "left-eye" :size 1}
+                            {:name "left-ear" :size 1}])
+
+
+(defn matching-part
+  [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+
+(defn symmetrize-body-parts
+  "Expects a seq of maps that have a :name and :size"
+  [asym-body-parts]
+  (loop [remaining-asym-parts asym-body-parts
+         final-body-parts []]
+    (if (empty? remaining-asym-parts)
+      final-body-parts
+      (let [[part & remaining] remaining-asym-parts]
+        (recur remaining
+               (into final-body-parts
+                     (set [part (matching-part part)])))))))
+
+(re-find #"^left-" "left-eye")
